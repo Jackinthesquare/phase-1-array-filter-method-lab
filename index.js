2af8e1daeafa71e9c5b7a078f1dc38c51a18e772
Jacky Liang <asheriffy@gmail.com>
2022-03-28 16:44:55 -0400

    commit tree f6ecf4e3ecfc10ce493840fa8294139eeac9b6bf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// const findMatching = (arr, str) => {
+//     const uppercased = arr.map(arr => arr.toUpperCase());
+//     return uppercased.filter(el => el === str.toUpperCase());
+// }
+
+function findMatching(arr,str){
+    return arr.filter(element => element.toUpperCase() === str.toUpperCase())
+}
+
+function fuzzyMatch(arr,str){
+    return arr.filter(element => element[0].toUpperCase() === str[0].toUpperCase())
+}
+
+function matchName(arr,str){
+    return arr.filter(element => element.home === str)
+    console.log(str)
+}
